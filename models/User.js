8fe79439b350ff809b8932308fd482a3bd93e2cc
JoeL <jlawton1203@gmail.com>
2020-09-01 14:37:41 -0500

    Added bcrypt and password encryption to User. Added user login route

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
-        //table column definitiions go here
         id:{
             type: DataTypes.INTEGER,
             allowNull: false,
@@ -33,7 +37,16 @@ User.init(
         }
     },
     {
-        //table configuration options go here
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
