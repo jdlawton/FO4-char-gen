2108c50166fb9f646c69141d980901d1f8af9370
JoeL <jlawton1203@gmail.com>
2020-09-03 21:31:58 -0500

    Started adding data to seeds file, completed first 75 perks. Added Handlebars for templating. Created initial homepage view and created single character-view which is able to display the basic character stats, as well as display the array of perk_ids for the perks that the character has taken.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,8 +46,64 @@ router.get('/login', (req, res) => {
         res.redirect('/');
         return;
     }
-    
     res.render('login');
+});
+
+router.get('/character/:id', (req, res) => {
+    Character.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'name',
+            'strength',
+            'perception',
+            'endurance',
+            'charisma',
+            'intelligence',
+            'agility',
+            'luck'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: CharacterPerk,
+                as: 'character_perks'
+            }
+        ]
+    })
+        .then(dbCharacterData => {
+            if (!dbCharacterData) {
+                res.status(404).json({message: 'No character found with this id'});
+                return;
+            }
+
+            const character = dbCharacterData.get({plain: true});
+            console.log(character);
+            console.log(character.id);
+            console.log(character.character_perks[0].perk_id);
+            /*return Perk.findOne({
+                where: {
+                    id: character.character_perks[0].perk_id
+                },
+                attributes: [
+                    'id',
+                    'name',
+                    'effect',
+                ]
+            });*/
+
+
+            res.render('character-view', {character});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
