207572d9a0a1e8c2f57c16ee4c3d7305e3baaf67
JoeL <jlawton1203@gmail.com>
2020-09-06 18:02:31 -0500

    When editing a character, the array of available perks now displays below the owned perks. Clicking on one of the available perks will open a modal which will display the perk name, rank, and effect. My goal is to add a button to add this perk to the modal which will then send a fetch request to the addperk path.

--- public/javascript/edit-character.js
+++ public/javascript/edit-character.js
@@ -1,3 +1,11 @@
+const perkWrapperEl = document.querySelector("#available-perk-wrapper");
+const modalPerkNameEl = document.querySelector("#modal-perk-name");
+const modalPerkRankEl = document.querySelector("#modal-perk-rank");
+const modalPerkEffectEl = document.querySelector("#modal-perk-effect");
+const spanPerkNameEl = document.querySelector("#span-perk-name");
+const spanPerkRankEl = document.querySelector("#span-perk-rank");
+const spanPerkEffectEl = document.querySelector("#span-perk-effect");
+
 //when a post id edited, and the update button is pressed, this function will send the PUT to the api to update the database with the new info.
 async function editFormHandler(event) {
     console.log("Inside editFormHandler");
@@ -35,4 +43,44 @@ async function editFormHandler(event) {
     }
 }
 
+//console.log(character);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const selectPerk = function (event) {
+    console.log(event);
+    //console.log(event.target);
+    //console.log(event.path[0].childNodes[1].innerHTML);
+    modalPerkNameEl.textContent = event.path[0].childNodes[1].innerHTML;
+    modalPerkRankEl.textContent = "Rank: " + event.path[0].childNodes[3].innerHTML;
+    modalPerkEffectEl.textContent = event.path[0].childNodes[6].innerHTML;
+    //console.log(modalPerkNameEl.textContent);
+    modal.style.display = "block";
+}
+
 document.querySelector('#edit-character-form').addEventListener('submit', editFormHandler);
+perkWrapperEl.addEventListener('click', selectPerk);
