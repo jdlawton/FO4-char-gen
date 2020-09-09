2e069d3d5ff4b09bb731a3c4951516cf177ac148
JoeL <jlawton1203@gmail.com>
2020-09-08 20:55:50 -0500

    Heroku issues

--- models/index.js
+++ models/index.js
@@ -5,49 +5,33 @@ const Character = require('./Character');
 const CharacterPerk = require('./CharacterPerk');
 
 //create associations
-User.hasMany(Character, {
-    foreignKey: 'user_id'
-});
+User.hasMany(Character);
 
-Character.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+Character.belongsTo(User);
 
-Dlc.hasMany(Perk, {
-    foreignKey: 'dlc_id'
-});
+Dlc.hasMany(Perk);
 
-Perk.belongsTo(Dlc, {
-    foreignKey: 'dlc_id'
-});
+Perk.belongsTo(Dlc);
 
 Character.belongsToMany(Perk, {
     through: CharacterPerk,
-    as: 'character_perks',
-    foreignKey: 'character_id'
+    as: 'character_perks'
+    //foreignKey: 'character_id'
 });
 
 Perk.belongsToMany(Character, {
     through: CharacterPerk,
-    as: 'character_perks',
-    foreignKey: 'perk_id'
+    as: 'character_perks'
+    //foreignKey: 'perk_id'
 })
 
-CharacterPerk.belongsTo(Character, {
-    foreignKey: 'character_id'
-});
+CharacterPerk.belongsTo(Character);
 
-Character.hasMany(CharacterPerk, {
-    foreignKey: 'character_id'
-});
+Character.hasMany(CharacterPerk);
 
-CharacterPerk.belongsTo(Perk, {
-    foreignKey: 'perk_id'
-});
+CharacterPerk.belongsTo(Perk);
 
-Perk.hasMany(CharacterPerk, {
-    foreignKey: 'perk_id'
-});
+Perk.hasMany(CharacterPerk);
 
 
 module.exports = {User, Dlc, Perk, Character, CharacterPerk};
